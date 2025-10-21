08fd9ca7946850a34d6c1fbca2036b9a52e10cd7
Ana Paula Morais <anapsmorais07@gmail.com>
2025-10-20 21:37:09 -0300

    Add files via upload
    2010

--- GerenciarVestidos.java
+++ GerenciarVestidos.java
@@ -16,58 +16,52 @@ import java.util.List;
     serviceName = "GerenciarVestidosService"
 )
 public class GerenciarVestidos {
-    private static final String URL = "jdbc:derby://localhost:1527/LAURA";
+    private static final String URL = "jdbc:derby://localhost:1527/laura";
     private static final String USUARIO = "laura";
     private static final String SENHA = "laura";
     private Connection conexao;
 
     @WebMethod(operationName = "verificarEmprestimo")
-    public boolean verificarEmprestimo(
-        @WebParam(name = "cor") String cor,
-        @WebParam(name = "tamanho") String tamanho,
-        @WebParam(name = "modelo") String modelo,
-        @WebParam(name = "id") int id,
-        @WebParam(name = "alugado") boolean alugado) {
+    public String verificarEmprestimo(
+        @WebParam(name = "id") int id)
+            {
         try {
             conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
-            String sql = "SELECT alugado FROM vestido WHERE cor = ? AND tamanho = ? AND modelo = ? AND id = ?  AND alugado = ?";
+            String sql = "SELECT alugado FROM vestidos WHERE id = ?";
             PreparedStatement localizar = conexao.prepareStatement(sql);
-            localizar.setString(1, cor);
-            localizar.setString(2, tamanho);
-            localizar.setString(3, modelo);
-            localizar.setInt(4, id);
-            localizar.setBoolean(5, alugado);
+            localizar.setInt(1, id);
             ResultSet resultado = localizar.executeQuery();
-            boolean estaAlugado = false;
+            String estaAlugado = "false";
             if (resultado.next()) {
-                estaAlugado = resultado.getBoolean("alugado");
+                estaAlugado = resultado.getString("alugado");
             }
             resultado.close();
             localizar.close();
             conexao.close();
-            return estaAlugado;
+            return "Augado";
         } catch (SQLException e) {
             e.printStackTrace();
-            return false;
+            return "Não alugado";
         }
     }
 
-    @WebMethod(operationName = "incluirVestido")
+
+    @WebMethod(operationName = "incluirVestidos")
     public boolean incluirVestido(
         @WebParam(name = "id") int id,
         @WebParam(name = "cor") String cor,
         @WebParam(name = "tamanho") String tamanho,
         @WebParam(name = "modelo") String modelo,
-        @WebParam(name = "alugado") int alugado) {
+        @WebParam(name = "alugado") boolean alugado) {
         try {
             conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
-            String sql = "INSERT INTO vestido (id, cor, tamanho, modelo,alugado) VALUES (?, ?, ?, ?, ?)";
+            String sql = "INSERT INTO vestidos (id,cor,tamanho,modelo,alugado) VALUES (?, ?, ?, ?, ?)";
             PreparedStatement incluir = conexao.prepareStatement(sql);
             incluir.setInt(1,id);
             incluir.setString(2, cor);
             incluir.setString(3, tamanho);
             incluir.setString(4, modelo);
-            incluir.setInt(5, alugado);
+            incluir.setBoolean(5, alugado);
             int linhasAfetadas = incluir.executeUpdate();
             incluir.close();
             conexao.close();
@@ -78,33 +72,26 @@ public class GerenciarVestidos {
         }
     }
 
-    @WebMethod(operationName = "alterarVestido")
-    public boolean alterarVestido(
+    @WebMethod(operationName = "alterarVestidos")
+public boolean alterarVestido(
+    
+    
+   
         @WebParam(name = "id") int id,
-        @WebParam(name = "cor") String cor,
-        @WebParam(name = "tamanho") String tamanho,
-        @WebParam(name = "modelo") String modelo,
-        @WebParam(name = "alugado") int alugado) {
+           @WebParam(name = "alugado") int alugado){
         try {
             conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
-            String sql = "UPDATE vestido SET cor = ?, tamanho = ?, modelo = ?, alugado = ? WHERE id = ?";
+            String sql = "UPDATE  vestidos SET alugado=? WHERE id=?";
             PreparedStatement alterar = conexao.prepareStatement(sql);
-            alterar.setString(1, cor);
-            alterar.setString(2, tamanho);
-            alterar.setString(3, modelo);
-            alterar.setInt(4, alugado);
-         
-
+            alterar.setInt(1,alugado);
+            alterar.setInt(2, id);
             int linhasAfetadas = alterar.executeUpdate();
             alterar.close();
             conexao.close();
-            return linhasAfetadas > 0; // true se alterou algo
+            return linhasAfetadas > 0;
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
         }
     }
-
-
-           
 }
